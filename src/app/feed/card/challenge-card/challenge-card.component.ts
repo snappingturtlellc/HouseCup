0ebcb7a7f79914da1d0b07f52c7cb136e9dd1e1c
Michael Sharp <michaelsharp@Michaels-Mac-mini.local>
2017-11-14 22:53:02 -0700

    cleaned up member-gallery

--- src/app/feed/card/challenge-card/challenge-card.component.ts
+++ src/app/feed/card/challenge-card/challenge-card.component.ts
@@ -3,6 +3,7 @@ import { FirebaseListObservable } from 'angularfire2/database';
 import { ChallengeService } from '../../../shared/service/challenge.service';
 import { AppService } from '../../../shared/service/app.service';
 import { IChallenge } from '../../../shared/class/challenge';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'gls-challenge-card',
@@ -15,7 +16,8 @@ export class ChallengeCardComponent implements OnInit {
 
   constructor(
     private appService: AppService,
-    private challengeService: ChallengeService) { }
+    private challengeService: ChallengeService,
+    private router: Router) { }
 
   ngOnInit() {
     this.appService.houseObservable.subscribe(house => {
@@ -49,5 +51,9 @@ export class ChallengeCardComponent implements OnInit {
 
   challengeSelect(challenge: IChallenge) {
     this.appService.setCurrentChallenge(challenge);
+  }
+
+  goChallenges() {
+    this.router.navigate(["/challenges"]);
   }
 }
--- src/app/feed/widget/member-gallery/member-gallery.component.ts
+++ src/app/feed/widget/member-gallery/member-gallery.component.ts
@@ -1,56 +1,25 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
-import { MemberService } from '../../../shared/service/member.service';
+import { Component } from '@angular/core';
 import { FirebaseListObservable } from 'angularfire2/database';
-import { IMember } from '../../../shared/class/member';
 import { Router } from '@angular/router';
-import { HouseService } from '../../../shared/service/house.service';
 import { AppService } from '../../../shared/service/app.service';
-import { IUser } from '../../../shared/class/user';
+import { IMember } from '../../../shared/class/member';
 
 @Component({
   selector: 'gls-member-gallery',
   templateUrl: './member-gallery.component.html',
   styleUrls: ['./member-gallery.component.scss']
 })
-export class MemberGalleryComponent implements OnInit {
-
-  //members$: FirebaseListObservable<any[]> = null;
-  //users: IUser[] = [];
+export class MemberGalleryComponent {
 
   constructor(
-    private memberService: MemberService,
     private appService: AppService,
-    private houseService: HouseService,
     private router: Router) { }
 
-  ngOnInit() {
-    // this.appService.houseObservable.subscribe(house => {
-    //   console.log("house change detected" + (house != null))
-    //   this.loadMembers();
-    // });
-
-    // setTimeout(() => {
-    //   this.loadMembers();      
-    // });
+  goMembers() {
+    this.router.navigate(['/members']);
   }
 
-  // loadMembers() {
-  //   console.log("load members: " + (this.appService.house != null))
-  //   if (this.appService.house) {
-  //     if (this.members$ == null)
-  //       this.members$ = this.memberService.getAll();
-  //   }
-  //   else {
-  //     this.members$ = null;
-  //   }
-  // }
-
-  add() {
-    this.router.navigate(['/member', 'new']);
-  }
-
-  edit(member: IMember) {
-    //this.router.navigate(['/member', member.$key]);
+  addPoints(member: IMember) {
     this.appService.showPointsDialog(member);
   }
 }
